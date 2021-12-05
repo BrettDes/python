b60dd770b114de21931367fc50c7978a51bb8acb
BrettDes <93301477+BrettDes@users.noreply.github.com>
2021-12-05 15:08:03 -0600

    12/5/21

---
+++ survey/Surveycode.py
@@ -0,0 +1,22 @@
+'''
+Created on Dec 5, 2021
+
+@author: ITAUser
+'''
+''' The Objective is to make a program that can take survey information
+from a user. '''
+
+'''To do list: 
+create variables with inputs and questions attached to them,
+run program and answer the questions to prove the code is functional,
+Submit the code to github and turn it into canvas.'''
+
+name= input("what is your name?")
+
+color= input("what is your favorite color?")
+
+city= input ("what city did you grow up in?")
+
+friend= input("what is your best friends name?")
+
+hair= input("what color hair do you have?")
