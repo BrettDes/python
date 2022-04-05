3447e05382bd569f5bed96c1df021e8f9a1ae8d6
BrettDes <93301477+BrettDes@users.noreply.github.com>
2022-04-04 21:29:14 -0500

    4/4/2022

---
+++ loops/loopOutline.py
@@ -0,0 +1,47 @@
+'''
+This outline will help solidify concepts from the Variables lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make a int variable called x set to 0. Make a while loop that prints 
+#out x and increments it by one. Make the loop run while x is less than
+#or equal to 20.
+x=0
+while(x=<20):
+print(x)
+
+
+#2) Make a int variable called x set to 0. Make a while loop that prints 
+#out x and increments it by one. Add an if statement that breaks the loop
+#if x equals 15. Make the loop run while x is less than 100.
+x=0
+while(x<100)
+print(x)
+x=x+x
+if(x==15): break
+#3) Make a int list with three items inside. Make a for loop that prints 
+#out all the items.
+a=[1,2,3]
+for x in a:
+print(x)
+
+#4) Make a string list with three items inside: "Michael", "Chris", "Nino". 
+#Make a for loop that prints out all the items. Add an if statement that 
+#breaks the loop if the item equals "Chris".
+b=["Michael","Chris","Nino"]
+for x in b:
+print(x)
+if(x=="Chris")break 
+#5) Make a while loop with a common SYNTAX error.
+x=0
+(x<100)
+print(x)
+x=x+x
+if(x==15): break
+
+
+#6) Make a for loop with a common SYNTAX error.
+a=[1,2,3]
+for x in a
+print(x)
+
